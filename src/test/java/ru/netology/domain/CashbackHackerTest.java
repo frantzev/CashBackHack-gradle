a0a2b00084926d8d1f327ac3aa7c05bfade05e96
Францев Алексей Евгеньевич <frantsev_ae@kms.tvz>
2021-12-19 12:15:13 +0300

    next commit testihg

--- src/test/java/ru/netology/domain/CashbackHackerTest.java
+++ src/test/java/ru/netology/domain/CashbackHackerTest.java
@@ -1,38 +1,38 @@
 package ru.netology.domain;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
 public class CashbackHackerTest {
     public CashbackHacker cashbackHacker = new CashbackHacker();
 
     @Test
-    void shouldCashBackSum900() {
+    public void shouldCashBackSum900() {
         int actual = cashbackHacker.remain(900);
         int expected = 100;
         assertEquals(expected, actual);
     }
     @Test
-    void shouldCashBackSum100() {
+    public void shouldCashBackSum100() {
         int actual = cashbackHacker.remain(100);
         int expected = 900;
         assertEquals(expected,actual);
     }
     @Test
-    void shouldCashBackSum5000() {
+    public void shouldCashBackSum5000() {
         int actual = cashbackHacker.remain(4000);
         int expected = 0;
         assertEquals(expected,actual);
     }
     @Test
-    void shouldCashBackSum1000() {
+    public void shouldCashBackSum1000() {
         int actual = cashbackHacker.remain(1000);
         int expected = 0;
         assertEquals(expected,actual);
     }
     @Test
-    void shouldCashBackSum1100() {
+    public void shouldCashBackSum1100() {
         int actual = cashbackHacker.remain(1100);
         int expected = 900;
         assertEquals(expected,actual);
