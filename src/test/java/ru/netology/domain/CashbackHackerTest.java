8349374ca8e0410260376e9963b6cdec541ec204
frantzev <80710458+frantzev@users.noreply.github.com>
2021-12-31 18:08:19 +0300

    Update CashbackHackerTest.java

--- src/test/java/ru/netology/domain/CashbackHackerTest.java
+++ src/test/java/ru/netology/domain/CashbackHackerTest.java
@@ -5,33 +5,39 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
 class CashbackHackServiceTest {
-    public CashbackHackService cashbackHacker = new CashbackHackService();
-
-    @Test
-    void shouldCashBackSum1100( ) {
-        int actual = cashbackHacker.remain(1100);
-        int expected = 900;
-        assertEquals(actual, expected);
+     @Test
+    public void enterIntegerValue() {
+        final CashbackHackService cashback = new CashbackHackService();
+        int amount = 5700;
+        int actual = cashback.remain(amount);
+        int expected = 300;
+        assertEquals(actual,expected);
     }
 
     @Test
-    void shouldCashBackSum500( ) {
-        int actual = cashbackHacker.remain(500);
-        int expected = 500;
-        assertEquals(actual, expected);
+    public void enterNullValue() {
+        final CashbackHackService cashback = new CashbackHackService();
+        int amount = 0;
+        int actual = cashback.remain(amount);
+        int expected = 1000;
+        assertEquals(actual,expected);
     }
 
     @Test
-    void shouldCashBackSum1000( ) {
-        int actual = cashbackHacker.remain(1000);
+    public void enterRoundValue() {
+        final CashbackHackService cashback = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashback.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(actual,expected);
     }
 
     @Test
-    void shouldCashBackSum3900( ) {
-        int actual = cashbackHacker.remain(3900);
-        int expected = 100;
-        assertEquals(actual, expected);
+    public void enterNegativeValue() {
+        final CashbackHackService cashback = new CashbackHackService();
+        int amount = -50000;
+        int actual = cashback.remain(amount);
+        int expected = 0;
+        assertEquals(actual,expected);
     }
 }
