65278447594d7f92a11743f91edddbef94ceb3e1
frantzev <frantzev@yandex.ru>
2022-01-12 23:08:21 +0300

    next commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,37 +2,31 @@ package ru.netology.service;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert;
+import  org.testng.Assert;
 
 public class CashbackHackServiceTest {
 
     @org.testng.annotations.Test
-    public void shouldReturnIfAmountIs800() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 500;
-        int actual = service.remain(amount);
-        int expected = 500;
-
-        Assert.assertEquals(actual, expected);
+    public void testRemainIfLess1000() {
+        int amount = 900;
+        int expected = 100;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        org.testng.Assert.assertEquals(cashbackHackService.remain(amount), expected);
     }
 
     @org.testng.annotations.Test
-    public void shouldReturnIfAmountIs1000() {
-        CashbackHackService service = new CashbackHackService();
+    public void testRemainIfEqual1000() {
         int amount = 1000;
-        int actual = service.remain(amount);
         int expected = 0;
-
-        Assert.assertEquals(actual, expected);
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        org.testng.Assert.assertEquals(cashbackHackService.remain(amount), expected);
     }
 
     @org.testng.annotations.Test
-    public void shouldReturnIfAmountIs0() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 0;
-        int actual = service.remain(amount);
-        int expected = 1000;
-
-        Assert.assertEquals(actual, expected);
+    public void testRemainIfMore1000() {
+        int amount = 1200;
+        int expected = 800;
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        org.testng.Assert.assertEquals(cashbackHackService.remain(amount), expected);
     }
 }
