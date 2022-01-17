565bdc2ea1ff249a5f42473a49254bc1186ff16f
frantzev <80710458+frantzev@users.noreply.github.com>
2022-01-18 01:02:48 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -6,31 +6,68 @@ import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
+   CashbackHackService service = new CashbackHackService();
+
     @Test
-    public void shouldReturn500() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 500;
-        int expected = 500;
+    public void shouldReturn100IfAmountIs900() {
+        int amount = 900;
         int actual = service.remain(amount);
+        int expected = 100;
+
         assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldReturn1000() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 0;
-        int expected = 1000;
-        int actual = service.remain(amount);
-        assertEquals(expected, actual);
-    }
-
-
-    @Test
-    public void shouldReturn0() {
-        CashbackHackService service = new CashbackHackService();
+    public void shouldReturnZeroIfAmountIs1000() {
         int amount = 1000;
-        int expected = 0;
         int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn500IfAmountIs1500() {
+        int amount = 1500;
+        int actual = service.remain(amount);
+        int expected = 500;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturn1000IfAmountIsZero() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateRemainIfAmountIsLessBoundary() {
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateRemainIfAmountIsMoreBoundary() {
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateRemainIfAmountIsLessZero() {
+        int amount = -1;
+        int actual = service.remain(amount);
+        int expected = 1001;
+
         assertEquals(expected, actual);
     }
 }
