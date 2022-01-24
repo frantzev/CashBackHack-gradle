cb9c5ee273dc8d168c693e77f45b831afc03d675
Францев Алексей Евгеньевич <frantsev_ae@kms.tvz>
2022-01-24 19:00:00 +0300

    next commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,6 @@
 package ru.netology.service;
 
 import org.junit.Assert;
-import org.junit.Test;
 
 public class CashbackHackServiceTest {
 
@@ -27,6 +26,7 @@ public class CashbackHackServiceTest {
 
    @org.junit.Test
     public void shouldReturn500IfAmountIs1500() {
+       CashbackHackService service = new CashbackHackService();
         int amount = 1500;
         int actual = service.remain(amount);
         int expected = 500;
@@ -36,6 +36,7 @@ public class CashbackHackServiceTest {
 
     @org.junit.Test
     public void shouldReturn1000IfAmountIsZero() {
+        CashbackHackService service = new CashbackHackService();
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
@@ -45,6 +46,7 @@ public class CashbackHackServiceTest {
 
     @org.junit.Test
     public void shouldCalculateRemainIfAmountIsLessBoundary() {
+        CashbackHackService service = new CashbackHackService();
         int amount = 999;
         int actual = service.remain(amount);
         int expected = 1;
@@ -54,6 +56,7 @@ public class CashbackHackServiceTest {
 
    @org.junit.Test
     public void shouldCalculateRemainIfAmountIsMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
         int amount = 1001;
         int actual = service.remain(amount);
         int expected = 999;
@@ -63,6 +66,7 @@ public class CashbackHackServiceTest {
 
     @org.junit.Test
     public void shouldCalculateRemainIfAmountIsLessZero() {
+        CashbackHackService service = new CashbackHackService();
         int amount = -1;
         int actual = service.remain(amount);
         int expected = 1001;
